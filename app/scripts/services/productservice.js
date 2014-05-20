af61402d7e0735361f3a4247fc10956ab858335e
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-20 18:38:26 +0700

    create factory : yo angular:factory ProductService

---
+++ app/scripts/services/productservice.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('belajarYeomanApp')
+  .factory('ProductService', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+    // Public API here
+    return {
+      someMethod: function () {
+        return meaningOfLife;
+      }
+    };
+  });
