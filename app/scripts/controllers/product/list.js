05c22709d1562897e4bdc7275be52ae44a3eddfd
Endy Muhardin <endy.muhardin@gmail.com>
2014-05-20 19:13:53 +0700

    implement edit view

--- app/scripts/controllers/product/list.js
+++ app/scripts/controllers/product/list.js
@@ -2,5 +2,10 @@
 
 angular.module('belajarYeomanApp')
   .controller('ProductListCtrl', function ($scope, ProductService) {
-    $scope.products = ProductService.query();
+    $scope.products = ProductService.query()
+    $scope.edit = function(x){
+    	$scope.selectedProduct = ProductService.get({id: x.id}, function(hasil){
+    		$scope.original = angular.copy(hasil);
+    	});
+    }
   });
--- app/scripts/services/productservice.js
+++ app/scripts/services/productservice.js
@@ -4,6 +4,9 @@ angular.module('belajarYeomanApp')
   .factory('ProductService', function ($resource) {
     return {
       productResource: $resource('api/product/:id'),
+      get: function(param, callback){
+        return this.productResource.get(param,callback);
+      },
       query: function(){
         return this.productResource.query();
       }
